df56fbf69c92bb9fc046f812605428c099ec577b
251351021-creator <251351021@wastukancana.ac.id>
2026-07-08 10:03:09 +0700

    change profile forms and dropdown to dark mode theme

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-slate-900 text-white border-gray-800 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
